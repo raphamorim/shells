bd94bef1a0d3fa1328e3a76aa3235a877168f34a
Raphael Amorim <rapha850@gmail.com>
2023-03-16 21:49:18 +0100

    Create parallel.sh

---
+++ parallel.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+MAX_CHILDREN=4
+
+function parallel {
+  local time1=$(date +"%H:%M:%S")
+  local time2=""
+
+  echo "starting $2 ($time1)..."
+  "$@" && time2=$(date +"%H:%M:%S") && echo "finishing $2 ($time1 -- $time2)..." &
+
+  local my_pid=$$
+  local children=$(ps -eo ppid | grep -w $my_pid | wc -w)
+  children=$((children-1))
+  if [[ $children -ge $MAX_CHILDREN ]]; then
+    wait -n
+  fi
+}
+
+parallel echo 1
+parallel echo 2
+parallel echo 3
+wait
